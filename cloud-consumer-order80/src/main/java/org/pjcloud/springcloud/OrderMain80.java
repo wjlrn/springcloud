d16dbd378251f1532cf0afb8cd4a50e379ba875b
wjlrn <renning.Lee@gmail.com>
2021-03-21 19:01:55 +0800

    单机Eureka构建

--- cloud-consumer-order80/src/main/java/org/pjcloud/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/org/pjcloud/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package org.pjcloud.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wanajinae
  **/
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-provider-payment8001/src/main/java/org/pjcloud/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/org/pjcloud/springcloud/PaymentMain8001.java
@@ -2,11 +2,13 @@ package org.pjcloud.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wanajinae
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
