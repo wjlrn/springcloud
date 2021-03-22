d800603a376fa65eca82962d2cdad8ca39833819
wjlrn <renning.Lee@gmail.com>
2021-03-23 00:10:08 +0800

    [Description]Eureka集群构建

---
+++ cloud-eureka-server7002/src/main/java/org/pjcloud/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package org.pjcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wanajinae
+ **/
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
