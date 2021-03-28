9bfa6c2d50108c271766c318a4fb7ce8238e16d8
wjlrn <renning.Lee@gmail.com>
2021-03-29 00:01:17 +0800

    [Description]支付服务注册进Zookeeper

---
+++ cloud-provider-payment8004/src/main/java/org/pjcloud/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package org.pjcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wanajinae
+ **/
+
+@SpringBootApplication
+//该注解用于向使用consul或zookeeper作为注册中心时注册服务
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/org/pjcloud/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package org.pjcloud.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author wanajinae
+ **/
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymenttzk(){
+        return "SpringCloud with zookeeper:" +serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
