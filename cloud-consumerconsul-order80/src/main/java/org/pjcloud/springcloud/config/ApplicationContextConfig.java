6682d949eeede76c975d7c76e14fba58c2234086
wjlrn <renning.Lee@gmail.com>
2021-04-04 09:06:03 +0800

    [Description]服务注册Consul

---
+++ cloud-consumerconsul-order80/src/main/java/org/pjcloud/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package org.pjcloud.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author wanajinae
+ **/
+
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
+//applicationContext.xml <bean id="" class="">
+
