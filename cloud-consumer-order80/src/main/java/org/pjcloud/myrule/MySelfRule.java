3cabc564a036e1dec79c96c5c085617d7b59ace5
wjlrn <renning.Lee@gmail.com>
2021-04-05 10:46:32 +0800

    [Description]ribbon负载均衡

---
+++ cloud-consumer-order80/src/main/java/org/pjcloud/myrule/MySelfRule.java
@@ -0,0 +1,17 @@
+package org.pjcloud.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wanajinae
+ **/
+@Configuration
+public class MySelfRule {
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();//定义为随机
+    }
+}
--- cloud-provider-payment8002/src/main/java/org/pjcloud/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/org/pjcloud/springcloud/controller/PaymentController.java
@@ -47,4 +47,9 @@ public class PaymentController {
             return new CommonResult(444, "没有对应记录，查询id:" + id, null);
         }
     }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB(){
+        return serverPort;
+    }
 }
