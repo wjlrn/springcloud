7b83c89290cc4cfe0d509ef25815c04caf97e2b7
wjlrn <renning.Lee@gmail.com>
2021-04-07 23:49:38 +0800

    [Description]hystrix服务降级

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/org/pjcloud/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,31 @@
+package org.pjcloud.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.pjcloud.springcloud.service.PaymentHystrixService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wanajinae
+ **/
+@RestController
+@Slf4j
+public class OrderHystrixController {
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return result;
+    }
+}
--- cloud-provider-hystrix-payment8001/src/main/java/org/pjcloud/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/org/pjcloud/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package org.pjcloud.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -20,13 +22,21 @@ public class PaymentService {
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_OK,id: "+ id+"\t"+"haha";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 3;
+        int timeNumber = 5;
+        //int age = 10/0;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOut,id: "+ id+"\t"+"haha" + " 耗时"+timeNumber+"秒钟";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOut,id: "+ id+"\t"+"/(ㄒoㄒ)/~~";
     }
 }
