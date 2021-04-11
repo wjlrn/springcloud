e9fa0ade5f75dd39019149e2fe7a0a3dd36932c6
wjlrn <renning.Lee@gmail.com>
2021-04-11 22:57:03 +0800

    [Description]hystrix服务监控Dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/org/pjcloud/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,16 @@
+package org.pjcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author wanajinae
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
