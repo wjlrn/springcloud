a2d0d8f4349ed8a12619a05ff4ca05bda30043b8
wjlrn <renning.Lee@gmail.com>
2021-04-12 23:39:09 +0800

    [Description]gateWay微服务网关代码配置实现

---
+++ cloud-gateway-gateway9527/src/main/java/org/pjcloud/springcloud/config/GateWayConfig.java
@@ -0,0 +1,21 @@
+package org.pjcloud.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wanajinae
+ **/
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_baidu",
+                r->r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+        return  routes.build();
+    }
+}
