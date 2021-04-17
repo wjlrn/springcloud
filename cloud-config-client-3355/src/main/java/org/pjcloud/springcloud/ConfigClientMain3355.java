380cb33ce18cfeb3a59eb848fe887f8390d457d8
wjlrn <renning.Lee@gmail.com>
2021-04-17 14:38:47 +0800

    [Description]Config服务配置中心客户端

---
+++ cloud-config-client-3355/src/main/java/org/pjcloud/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,16 @@
+package org.pjcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wanajinae
+ **/
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
