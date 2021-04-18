fe6299d247c9ac7f34bc2208b81fec8275d074b3
wjlrn <renning.Lee@gmail.com>
2021-04-18 11:07:30 +0800

    [Description]Bus消息总线

---
+++ cloud-config-client-3366/src/main/java/org/pjcloud/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package org.pjcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wanajinae
+ **/
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
