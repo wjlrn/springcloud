dea6502f28140394efb7de727cd2606b6c265137
wjlrn <renning.Lee@gmail.com>
2021-04-19 22:44:58 +0800

    [Description]Stream消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/org/pjcloud/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package org.pjcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wanajinae
+ **/
+
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
