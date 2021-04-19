dc5086b1d7a3518dbd3676274302872bc3e6d7de
wjlrn <renning.Lee@gmail.com>
2021-04-19 23:32:46 +0800

    [Description]Stream消息驱动之消息重复消费持久化等问题处理

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/org/pjcloud/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package org.pjcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wanajinae
+ **/
+
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/org/pjcloud/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,24 @@
+package org.pjcloud.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wanajinae
+ **/
+
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println("消费者2号，---->接收的消息："+message.getPayload()+"\t prot："+serverPort);
+    }
+}
