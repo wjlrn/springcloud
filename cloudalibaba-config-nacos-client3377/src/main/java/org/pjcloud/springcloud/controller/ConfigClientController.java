addb90a2bcb008543a28078c5abf73b8eb8383c3
wjlrn <renning.Lee@gmail.com>
2021-06-20 22:04:56 +0800

    [Description]Nacos配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/org/pjcloud/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package org.pjcloud.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wanajinae
+ **/
+@RestController
+@RefreshScope  //支持nacos的动态刷新功能
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
